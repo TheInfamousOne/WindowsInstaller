050a0c2c11cff1304cf4fc2cd6e16485881b00ea
Ty Stallard <tyrones@med.umich.edu>
2015-05-21 08:35:01 -0400

    WindowsInstaller
    This works with ZapIt and the WindowsInstaller.exe must be in the same
    directory as ZapIt to work.  I'm sure this could have been written
    better, but it was one of me first projects.

---
+++ WindowsInstaller/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Deployment.WindowsInstaller;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace WindowsInstaller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                           
+                IEnumerable<ProductInstallation> installations = Microsoft.Deployment.WindowsInstaller.ProductInstallation.GetProducts(null, "s-1-1-0", UserContexts.All);
+               
+
+                // Step 3 - Loop through the installed MSI Products and output information
+
+                while (true)
+                {
+                    foreach (ProductInstallation installation in installations)
+                    {
+                        Console.WriteLine(installation.ProductName);                        
+                        Console.WriteLine(installation.ProductCode);
+                        
+                         
+                    }
+                    return;
+                    
+                    
+                    
+                }
+                    
+            }           
+                              
+          }        
+        
+    }
+
+
